c95c9c2cd8ccfcdcd0cbcef8c5d9c2aea7f0a1eb
macun <macun@yonyou.com>
2019-07-03 09:26:55 +0800

    eposide组件

--- components/eposide/index.js
+++ components/eposide/index.js
@@ -4,14 +4,40 @@ Component({
    * 组件的属性列表
    */
   properties: {
+    index: {
+      type: Number,
+      observer: function (newVal, oldVal, changePath) {
+        let _index = ''
+        if (newVal < 10) {
+          _index = `0${newVal}`
+        } else {
+          _index = newVal
+        }
+        this.setData({
+          _index: `0${newVal}`
+        })
 
+      }
+    }
+  },
+
+  attached: function () {
+    const date = new Date()
+    const year = date.getFullYear()
+    const month = date.getMonth()
+    this.setData({
+      year,
+      month: `${this.data.months[month]}月`
+    })
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    months: ["一", '二', '三', '四', '五', '六', '七', '八', '九', '十', '十一', '十二'],
+    year: 0,
+    month: ""
   },
 
   /**
